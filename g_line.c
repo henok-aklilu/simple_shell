deb0c919810e780263b24f4467e1173190ea8778
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2023-02-22 12:48:52 -0800

    line

--- g_line.c
+++ g_line.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * bring_line - assigns the line var for get_line
+ * bring_line - assigns the line var for the get_line
  * @lineptr: Buffer that store the input str
  * @buffer: str that is been called to line
  * @n: size of line
